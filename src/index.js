8c30a31cd553df49e99aeef71d1758243df3d69b
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-12-09 00:35:06 +0200

    hw-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+
+import { fetchCountries } from './fetchCountries';
+
+// -----Инициализация-------------
+
+const input = document.getElementById("search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
 
 const DEBOUNCE_DELAY = 300;
+
+
+input.addEventListener("input", debounce((event) => {
+    const inputValue = event.target.value.trim();
+
+    if (inputValue === '') {
+        return;
+    }
+
+    fetchCountries(inputValue)
+        .catch(() => {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        })
+        .then(list => {
+            if (list.length > 10) {
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            } else if (list.length > 1) {
+                renderCountryList(list);
+            } else if (list.length == 1) {
+                renderCountryInfo(list[0]);
+            }
+        });
+}, DEBOUNCE_DELAY));
+
+function renderCountryList(list) {
+    const markup = list
+        .map(item => {
+            return `<li>
+            <img src="${item.flags.svg}"  width="50px" />
+            ${item.name.official}
+            </li>`;
+        })
+        .join("");
+    countryInfo.innerHTML = "";
+    countryList.innerHTML = markup;
+}
+
+function renderCountryInfo(item) {
+
+    const markup = `
+            <h1>
+            <img src="${item.flags.svg}"  width="50px"/>
+            ${item.name.official}
+            </h1>
+            <p><b>Capital</b>: ${item.capital}</p>
+            <p><b>Population</b>: ${item.population}</p>
+            <p><b>Languages</b>: ${Object.values(item.languages)}</p>
+        `;
+    countryList.innerHTML = "";
+    countryInfo.innerHTML = markup;
+}
